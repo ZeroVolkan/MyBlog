72c703de797c30b9c982cad6f6a78dec0b771ab9
ZeroVolkan <volkan@github.com>
2024-07-20 00:12:55 +0400

    add_basic_css

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Query, Header
+from fastapi import FastAPI, Query, Header, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates =  Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def main(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
+
+
 
 
 @app.get("/blogs/{blog_id}", tags=["blogs"])
